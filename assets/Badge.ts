2981708215ab2123cac0bf438c78b5b43c17e71d
brad <44528100+pozm@users.noreply.github.com>
2021-06-06 14:11:34 +0100

    stfu eslint

--- assets/Badge.ts
+++ assets/Badge.ts
@@ -4,7 +4,7 @@ export namespace Badge {
     export function has (n:number) {
         const arr = []
         for (const [idx] of Object.entries(userData.Badges)) {
-            if (+idx & n)
+            if (!!(+idx & n)) // eslint-disable-line -- stfu
                 arr.push(+idx)
         }
         return arr
